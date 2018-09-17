2946bcc58912fddfa7ba7d316bac413f296ca2c1
CaoQian <afewgoodken@icloud.com>
2018-09-17 14:28:03 +0800

    add UI to viewController

---
+++ test5/AddViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddViewController.swift
+//  test5
+//
+//  Created by CaoQian on 2018/09/17.
+//  Copyright © 2018 CaoQian. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tapBtn(_ sender: UIButton) {
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
