1a5b3bffbadb9f80a5aa031ffe361ed47781ae91
CaoQian <afewgoodken@icloud.com>
2018-09-17 15:03:13 +0800

    create tableView

--- test5/AddViewController.swift
+++ test5/AddViewController.swift
@@ -13,26 +13,12 @@ class AddViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func tapBtn(_ sender: UIButton) {
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       //navigationCotrollerを使ったときに元の画面に戻る書き方
+        self.navigationController?.popViewController(animated: true)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
